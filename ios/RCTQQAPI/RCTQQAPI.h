60f863223fd31b1a383bba135d0047e5c96aa9ff
LvBingru <lvbingru@gmail.com>
2015-12-18 04:22:00 +0800

    iOS module

---
+++ ios/RCTQQAPI/RCTQQAPI.h
@@ -0,0 +1,29 @@
+//
+//  RCTQQAPI.h
+//  RNThirdShareMangager
+//
+//  Created by LvBingru on 10/10/15.
+//  Copyright © 2015 erica. All rights reserved.
+//
+
+#import "RCTBridgeModule.h"
+
+
+#define RCTQQShareTypeNews @"news"
+#define RCTQQShareTypeImage @"image"
+#define RCTQQShareTypeText @"text"
+#define RCTQQShareTypeVideo @"video"
+#define RCTQQShareTypeAudio @"audio"
+
+#define RCTQQShareType @"type"
+#define RCTQQShareTitle @"title"
+#define RCTQQShareDescription @"description"
+#define RCTQQShareWebpageUrl @"webpageUrl"
+#define RCTQQShareImageUrl @"imageUrl"
+
+@interface RCTQQAPI : NSObject<RCTBridgeModule>
+
++ (void)registerAPI:(NSString *)aString;
++ (BOOL)handleUrl:(NSURL *)aUrl;
+
+@end

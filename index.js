00c50224950c282aa91362dbcf4ca1b8d1c9a1d7
liudeng <liudeng@ffrj.net>
2016-09-21 17:28:32 +0800

    fix

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var React = require('react-native');
 var Platform = React.Platform;
 
 if (Platform.OS === 'android') {
-  module.exports = require('./index.android.ios');
+  module.exports = require('./index.android');
 } else if (Platform.OS === 'ios') {
-  module.exports = require('./index.ios.ios');
+  module.exports = require('./index.ios');
 }

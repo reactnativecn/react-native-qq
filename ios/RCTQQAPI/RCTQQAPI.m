b1b8ce1de572f3e872d0ccae003294a77febfd53
Nevo <nevo.lee@gmail.com>
2018-05-23 13:06:35 +0800

    fix issue: RCTEventEmitter bridge is nil

--- ios/RCTQQAPI/RCTQQAPI.m
+++ ios/RCTQQAPI/RCTQQAPI.m
@@ -29,7 +29,7 @@
 
 @implementation RCTQQAPI
 
-@synthesize bridge = _bridge;
+//@synthesize bridge = _bridge;
 
 RCT_EXPORT_MODULE();
 
@@ -124,13 +124,13 @@ RCT_EXPORT_METHOD(logout)
 
 - (void)_shareToQQWithData:(NSDictionary *)aData scene:(int)aScene resolve:(RCTPromiseResolveBlock)resolve reject:(RCTPromiseRejectBlock)reject{
     NSString *imageUrl = aData[RCTQQShareImageUrl];
-    if (imageUrl.length && _bridge.imageLoader) {
+    if (imageUrl.length && self.bridge.imageLoader) {
         CGSize size = CGSizeZero;
         if (![aData[RCTQQShareType] isEqualToString:RCTQQShareTypeImage]) {
             CGFloat thumbImageSize = 80;
             size = CGSizeMake(thumbImageSize,thumbImageSize);
         }
-        [_bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:imageUrl] callback:^(NSError *error, UIImage *image) {
+        [self.bridge.imageLoader loadImageWithURLRequest:[RCTConvert NSURLRequest:imageUrl] callback:^(NSError *error, UIImage *image) {
             dispatch_async(dispatch_get_main_queue(), ^{
                 [self _shareToQQWithData:aData image:image scene:aScene resolve:resolve reject:reject];
             });
